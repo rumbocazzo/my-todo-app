a64663290cc25a0cc7fe91108735c4a73a4f87b0
RichVos <118569904+rumbocazzo@users.noreply.github.com>
2023-09-18 13:15:33 -0400

    Initial commit

---
+++ web.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state["new_todo"] = "" # Clear the input text box
+
+st.title("My Todo App")
+st.subheader("Nice app, stupid.")
+st.write("This app is to increase your productivity but it most likely won't because it's fucking retarded.")
+
+for index, todo in enumerate(todos):
+    checkbox = st.checkbox(todo, key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[todo]
+        st.experimental_rerun()
+
+st.text_input(label="peckas", label_visibility='hidden', placeholder="Add a todo, dummy...",
+              on_change=add_todo, key="new_todo")
